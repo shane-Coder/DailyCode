f81b3c7b0859d18aacacabb87905e04ec22ba909
shIvam omer <75215089+shane-Coder@users.noreply.github.com>
2023-05-13 08:29:39 +0530

    Added solution - LeetHub

---
+++ Bit Magic - GFG/bit-magic.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int bitMagic(int n, vector<int> &ar) {
+        // code here
+        int low=0, high=n-1, ct=0;
+        while(low<high){
+            if(ar[low++]!=ar[high--])
+                ct++;
+        }
+        return (ct+1)/2;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        Solution obj;
+        int res = obj.bitMagic(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
